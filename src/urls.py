2617ae70ef5d75627d471939c360eb8b8a39cb42
shamspias <shamsuddinpias0@gmail.com>
2022-10-11 15:41:21 +0600

    update urls

--- src/urls.py
+++ src/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.urls import path, re_path, include, reverse_lazy
 from django.conf.urls.static import static
-from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
@@ -17,7 +16,7 @@ from src.files.urls import files_router
 from src.users.urls import users_router
 
 schema_view = get_schema_view(
-    openapi.Info(title="Pastebin API", default_version='v1'),
+    openapi.Info(title="Jongli E-Commerce API", default_version='v1'),
     public=True,
 )
 
@@ -29,25 +28,24 @@ router.registry.extend(files_router.registry)
 urlpatterns = [
     # admin panel
     path('admin/', admin.site.urls),
-    url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
     # summernote editor
     path('summernote/', include('django_summernote.urls')),
     # api
     path('api/v1/', include(router.urls)),
-    url(r'^api/v1/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    re_path(r'^api/v1/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
     # auth
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # social login
-    url('', include('social_django.urls', namespace='social')),
-    url(r'^complete/twitter/', complete_twitter_login),
-    url(r'^api/v1/social/(?P<backend>[^/]+)/$', exchange_token),
+    re_path('', include('social_django.urls', namespace='social')),
+    re_path(r'^complete/twitter/', complete_twitter_login),
+    re_path(r'^api/v1/social/(?P<backend>[^/]+)/$', exchange_token),
     # swagger docs
-    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    url(r'^health/', include('health_check.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^health/$', include('health_check.urls')),
     # the 'api-root' from django rest-frameworks default router
     re_path(r'^$', RedirectView.as_view(url=reverse_lazy('api-root'), permanent=False)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
